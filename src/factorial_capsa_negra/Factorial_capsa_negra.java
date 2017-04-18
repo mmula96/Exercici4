cc783e83c31979a668c56f700b387930ffea10f4
cfgs <cfgs@pc-info-host>
2017-04-18 19:12:12 +0200

    Version inicial

---
+++ src/factorial_capsa_negra/Factorial_capsa_negra.java
@@ -0,0 +1,15 @@
+package factorial_capsa_negra;
+
+public class Factorial_capsa_negra {
+
+    private static int factorial(int n){
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args){
+        System.out.println(factorial(3));
+    }
+}
